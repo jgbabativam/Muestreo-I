6ae46b4a69da6b892c68b0857b2aabbb28af5831
gbabativam <gbabativam@gmail.com>
2024-11-22 19:29:01 -0500

    porojexts

---
+++ Projects/EjerMuest/src/1. Seleccion de la Muestra.R
@@ -0,0 +1,25 @@
+
+rm(list = ls())
+
+library(pacman)
+p_load(tidyverse, janitor, readxl, writexl)
+
+#### Tamaño de la muestra
+
+n <- 10
+
+#### Marco
+
+marco <- read_excel("data/Ejemplo Hogares - Marco Muestral.xls")
+
+set.seed(2024-11-22)
+marco_ord <- marco |> 
+             mutate(aleatorio = runif(n = nrow(marco))) |> 
+             arrange(aleatorio)
+
+save(marco_ord, file = "output/marco_ordenado.rds")
+
+muestra <- marco_ord |> 
+           slice(1:n)
+
+write_xlsx(muestra, "output/Muestra Seleccionada.xlsx")
